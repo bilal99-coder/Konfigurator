7a978a62792fc219c0812153516ea3a29bf77a87
bilal99-coder <56065152+bilal99-coder@users.noreply.github.com>
2020-03-31 22:18:49 +0200

    Update mainController.java
    Jeg har endret på noen av den navnene. Jeg har også byttet noen checkbuttons til radioButtons. Det er letter å håndtere radioButtons. 
    Jeg har også fikset den merke og model comboBox.

--- Konfigurator/src/Controller/mainController.java
+++ Konfigurator/src/Controller/mainController.java
@@ -1,61 +1,62 @@
 package Controller;
 
+import Model.Bil;
+import Model.Utstyr;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
-public class mainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable  {
+   // public boolean autoGir_check;
 
     @FXML
-    private ComboBox<String> merkeBox;
+    private ComboBox<String> merkeBox ;
 
     @FXML
-    private ComboBox<String> motorBox;
+    private ComboBox<String> modelBox;
+
+    @FXML
+    private ComboBox<String> drivstoffBox;
+
 
     @FXML
     private Slider hestSlider;
+    @FXML
+    private RadioButton autoGirBtn;
 
     @FXML
-    private CheckBox automatGirkasseBtn;
-
-    @FXML
-    private CheckBox manGirBtn;
+    private RadioButton manuellGirBtn;
 
     @FXML
     private TextField lblTotalPris;
-
     @FXML
-    void HengerFesteBtn(ActionEvent event) {
+    private TextField sliderValue;
+    @FXML
+    void hkSliderOnAction(MouseEvent event) {
+        //Henter ønsker hestkreft verdien og printer den ut i GUI
+        int hk_Gui = (int) hestSlider.getValue();
+        sliderValue.setText(hk_Gui+"");
 
     }
 
     @FXML
-    void NavigasjonPakkeBtn(ActionEvent event) {
-
-    }
+    private CheckBox HengerFeste_CheckBtn;
 
     @FXML
-    void autoGir(ActionEvent event) {
-
-    }
+    private CheckBox radioDab_checkBtn;
 
     @FXML
-    void dab_Btn(ActionEvent event) {
-
-    }
+    private CheckBox NavigasjonPakke_checkBtn;
 
     @FXML
-    void klimaBtn(ActionEvent event) {
+    private CheckBox klimapakke_checkBtn;
 
-    }
 
-    @FXML
-    void manGir(ActionEvent event) {
-
-    }
 
     @FXML
     void metalikkLakkBtn(ActionEvent event) {
@@ -66,33 +67,66 @@ public class mainController {
     void stnadardLakkBtn(ActionEvent event) {
 
     }
-       @Override
+
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
         merkeBox.getItems().addAll("Audi", "Volvo", "Nissan");
-        //modelBox.getItems().addAll("Velg en bil merke først!");
+        modelBox.getItems().addAll("Velg en bil merke først!");
         drivstoffBox.getItems().addAll("Elektrisitet", "Bensin", "Diesel", "Hybrid");
-        merkeBox.setOnAction(e-> changeModell());
-       // merkeBox.
+
+
    }
-   public void changeModell( ){
-       String Merke = merkeBox.getValue().toString();
-        if (merkeBox.getValue().toString().equals("Audi")){
-          //  modelBox.setValue(null);
+   public void changeModell( ActionEvent event ){
+       String Merke = merkeBox.getValue();
+       modelBox.getItems().clear();
+        if (merkeBox.getValue().equals("Audi")){
+
                modelBox.getItems().addAll("A3", "A5", "A6", "Q5","Q7", "Q8", "RS3");
         }
       else   if (Merke.equals("Volvo")){
-          merkeBox = new ComboBox<>() ;
+
             modelBox.getItems().addAll("XC40" , "XC60", "XC90", "S60");
 
         }
       else  if (Merke.equals("Nissan")){
-            merkeBox = new ComboBox<>() ;
             modelBox.getItems().addAll("Leaf", "Quashqai" ,"GTR" );
 
         }
        else
        modelBox.getItems().addAll("Velg en merke!");
 
+    }
+    public String girKasse(){
+        //Denne metoden henter valgt girkasse button fra Gui og returnere en streng som representerer girkasse type.
+        //gruppere JavaFX RadioButton-forekomster i en ToggleGroup.
+        ToggleGroup radioGruppe =  new ToggleGroup();
+        // En ToggleGroup lar deg velge kun en RadioButton når som helst.
+
+        autoGirBtn.setToggleGroup(radioGruppe);
+        manuellGirBtn.setToggleGroup(radioGruppe);
+        RadioButton rb = (RadioButton) radioGruppe.getSelectedToggle();
+        String girkasse = rb.getText();
+        return girkasse;
+
+
+    }
+   /* public Utstyr ekstra_Utstyr(){
+        Utstyr ekstra_Utstyr = new Utstyr();
+        if (HengerFeste_CheckBtn.isSelected()){
+           // ekstra_Utstyr.isHengerfeste()==  true;
+        }
+    }*/
+    public void register(){
+        //Henter bil merke fra comboBox fra GUI
+        String bilMerke = merkeBox.getValue();
+        // Henter bil model fra GUI
+        String bilModell = modelBox.getValue() ;
+        // Henter ønsket drivstoff fra GUI
+        String drivstoff = drivstoffBox.getValue();
+        // Henter ønsket hestkreft fra GUI
+        int hk_Gui = (int) hestSlider.getValue();
+
+      //  Bil nyBil = new Bil(bilMerke, bilModell,  drivstoff, hk_Gui, girKasse() , );
     }
 
 }
